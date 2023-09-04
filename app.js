99d6819d27d8ca73f87b8af07995c0bb56292ee9
tech.julian.salvador <tech.julian.salvador@gmail.com>
2023-09-04 17:37:17 +0800

    Capstone1

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const navBtn = document.querySelector(".menuBtn");
 const menuLineTop = document.querySelector(".menu-line-top");
 const menuLineCenter = document.querySelector(".menu-line-center");
 const menuLineBot = document.querySelector(".menu-line-bottom");
+const toTop = document.querySelector(".to-top");
 
+//Hamburger Button Animation
 navBtn.addEventListener("click", () => {
   menuLineTop.classList.toggle("menuToggled");
   menuLineCenter.classList.toggle("menuToggledCenter");
   menuLineBot.classList.toggle("menuToggled");
 });
 
-const themeBtn = document.querySelector("#themeSelectorContainer");
+// Back to Top
 
-themeBtn.addEventListener("click", () => {});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    toTop.classList.add("scroll-up");
+  } else {
+    toTop.classList.remove("scroll-up");
+  }
+});
+
+let readMore = document.querySelector(".read-more");
+readMore.addEventListener("click", () => {
+  readMore.parentNode.classList.toggle("expand-bio");
+});
+
+//clear fields upon refresh of page
+window.onload = function () {
+  // Reset input fields
+  var inputFields = document.querySelectorAll("input");
+  for (var i = 0; i < inputFields.length; i++) {
+    inputFields[i].value = "";
+  }
+};
